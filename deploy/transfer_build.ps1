8bb85ea889f83293b2f1c105b055e4fc165b656f
Christian-Schefe <schefechristian@gmail.com>
2025-08-30 12:21:53 +0200

    ci: change server structure

--- deploy/transfer_build.ps1
+++ deploy/transfer_build.ps1
@@ -3,6 +3,6 @@ echo "Building..."
 Remove-Item -Recurse -Force dist
 bun run build
 echo "Transferring files to server..."
-ssh tak_server "rm -rf /root/app/dist"
+ssh tak_server "rm -rf /root/app/dist/dist"
 sleep 1
-scp -r dist tak_server:/root/app
+scp -r dist tak_server:/root/app/dist
--- deploy/transfer_files.ps1
+++ deploy/transfer_files.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 echo "Transferring files to server..."
-ssh tak_server "mkdir -p /root/app/certs"
+ssh tak_server "mkdir -p /root/app/certs && mkdir -p /root/app/deploy"
 sleep 1
 scp certs/certificate.pem certs/key.pem tak_server:/root/app/certs/
 sleep 1
-scp deploy/docker-compose.yml deploy/Caddyfile tak_server:/root/app/
+scp deploy/docker-compose.yml deploy/Caddyfile tak_server:/root/app/deploy
